408062cf5f713f3ce599d7f5521899c61eb7efd7
OscarTulkens <43540793+OscarTulkens@users.noreply.github.com>
2018-12-10 23:15:54 +0100

    GP1CG-66-add-unit-spawning.
    GP1CG-66-add-unit-spawning.

---
+++ Unity/Team03_Virtuabattle/Assets/Scripts/SpawnUnitsScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnUnitsScript : MonoBehaviour {
+
+    public float CoolDownTime;
+    private float _timer;
+    private GameObject _lastSpwanedUnit;
+
+    //private AIPathControl _gameControl;
+
+    public enum TroopType
+    {
+        Land,
+        Sky,
+        Other
+    }
+    public TroopType troopType;
+
+    public GameObject LandPrefab;
+    public GameObject SkyPrefab;
+
+    private GameObject _unitPrefab;
+    private int _goldCost;
+    public int TeamNumber;
+
+    public Vector3 _tempSpawnPos;
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        //_gameControl = GameObject.Find("Game Control").GetComponent<AIPathControl>();
+
+        //GetComponent<Renderer>().material = _gameControl.TeamMaterials[TeamNumber - 1];
+
+        //_tempSpawnPos = _gameControl.TeamSpawnPoints[TeamNumber - 1].position;
+
+        if (troopType == TroopType.Land)
+        {
+            _unitPrefab = LandPrefab;
+            _goldCost = 1;
+        }
+        else
+        {
+            _unitPrefab = SkyPrefab;
+            _goldCost = 3;
+        }
+        _timer = CoolDownTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _timer-= Time.deltaTime;
+	}
+
+    public int InitiateUnit(int TeamNumber)
+    {
+        if (_timer <= 0)
+        {
+
+            if (this.TeamNumber == TeamNumber)
+            {
+
+
+                _lastSpwanedUnit = Instantiate(_unitPrefab, _tempSpawnPos, Quaternion.identity);
+                //_lastSpwanedUnit.GetComponent<Renderer>().material = _gameControl.TeamMaterials[TeamNumber - 1];
+                //_lastSpwanedUnit.GetComponent<AIScript>().TeamNumber = TeamNumber;
+                //_lastSpwanedUnit.GetComponent<AIScript>().SpawnPoints = _gameControl.TeamSpawnPoints;
+                _timer = CoolDownTime;
+                return _goldCost;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        else
+        {
+            return 0;
+        }
+    }
+}
