4088fb7a6663bda508d95f5861f16547713be07d
Stickie <pieterdestickere@gmail.com>
2018-12-11 10:03:28 +0100

    GP1CG-70 - Improve CharacterController motion.
    GP1CG-70 - Improve CharacterController motion.

--- Unity/Team03_Virtuabattle/Assets/Scripts/CharController.cs
+++ Unity/Team03_Virtuabattle/Assets/Scripts/CharController.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
 
-//[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(CharacterController))]
 public class CharController : MonoBehaviour {
-
 
     [SerializeField] private float gravityMultiplier;
     [SerializeField] private float _acceleration;
@@ -19,129 +18,108 @@ public class CharController : MonoBehaviour {
     CharacterController _char;
     Animator _anim;
     InputController inputController;
-    Camera camera;
 
     [HideInInspector] public Vector3 Velocity = Vector3.zero; // [m/s]
     [HideInInspector] public Vector3 InputMovement;
     private bool _jump;
 
     void Start ()
-        {
+    {
         _char = GetComponent<CharacterController>();
-        camera = GetComponentInChildren<Camera>();
-        _absoluteTransform = camera.transform;
+        _absoluteTransform = GetComponentInChildren<Camera>().transform;
         inputController = GetComponent<InputController>();
 
         #if DEBUG
         Assert.IsNotNull(_char, "DEPENDENCY ERROR: CharacterController missing from PlayerScript");
         #endif
 
-        }
+    }
 
     private void Update()
     {
-        InputMovement = new Vector3(inputController.PlayerMovementHorizontal(), 0, inputController.PlayerMovementVertical()).normalized;
-        if (inputController.IsJumping())
-            {
-            _jump = true;
-            }
+        InputMovement = new Vector3(inputController.PlayerMovementHorizontal, 0, inputController.PlayerMovementVertical).normalized;
+        if (_char.isGrounded && inputController.Jump) _jump = true;
     }
 
     void FixedUpdate ()
-        {
-        ApplyGround();
+    {
+        if (_char.isGrounded) ApplyGround();
         ApplyGravity();
-        ApplyMovement();
-        ApplyDragOnGround();
-        ApplyJump();
+        //if (!_char.isGrounded) ApplyGravity();
+        if (_char.isGrounded) ApplyMovement();
+        if (_char.isGrounded) ApplyDragOnGround();
+        if (_char.isGrounded) ApplyJump();
         LimitXZVelocity();
 
         Vector3 XZvel = Vector3.Scale(Velocity, new Vector3(1, 0, 1));
         Vector3 localVelXZ = gameObject.transform.InverseTransformDirection(XZvel);
 
-
-
+        //if (Velocity != Vector3.zero) DoMovement();
         DoMovement();
+
+        //Reset downwards velocity if charactercontroller is grounded.
+        //if (_char.isGrounded && Velocity.y < 0)
+        //    Velocity.y = 0;
     }
 
 
     private Vector3 RelativeDirection(Vector3 direction)
-        {
+    {
 
         Vector3 xzForward = Vector3.Scale(_absoluteTransform.forward, new Vector3(1, 0, 1));
         Quaternion relativeRot = Quaternion.LookRotation(direction);
 
         return relativeRot.eulerAngles;
-        }
+    }
 
 
     private void ApplyGround()
-        {
-        if (_char.isGrounded)
-            {
-
-            Velocity -= Vector3.Project(Velocity, Physics.gravity);
-            }
-        }
+    {
+        Velocity -= Vector3.Project(Velocity, Physics.gravity);
+    }
 
     private void ApplyGravity()
-        {
-        if (!_char.isGrounded)
-            {
-
-            Velocity += (Physics.gravity * gravityMultiplier) * Time.deltaTime;
-            }
-        }
+    {
+        Velocity += (Physics.gravity * gravityMultiplier) * Time.deltaTime;
+    }
 
     private void ApplyMovement()
-        {
-        if (_char.isGrounded)
-            {
-
-            Vector3 xzForward = Vector3.Scale(_absoluteTransform.forward, new Vector3(1, 0, 1));
-            Quaternion relativeRot = Quaternion.LookRotation(xzForward);
-
-
-            Vector3 relativeMov = relativeRot * InputMovement;
-            Velocity += relativeMov * _acceleration * Time.deltaTime;
-            }
-
-        }
+    {
+        Vector3 xzForward = Vector3.Scale(_absoluteTransform.forward, new Vector3(1, 0, 1));
+        Quaternion relativeRot = Quaternion.LookRotation(xzForward);
+        Vector3 relativeMov = relativeRot * InputMovement;
+        Velocity += relativeMov * _acceleration * Time.deltaTime;
+    }
 
     private void LimitXZVelocity()
-        {
+    {
         Vector3 yVel = Vector3.Scale(Velocity, Vector3.up);
         Vector3 xzVel = Vector3.Scale(Velocity, new Vector3(1, 0, 1));
 
         xzVel = Vector3.ClampMagnitude(xzVel, _maximumXZVelocity);
 
         Velocity = xzVel + yVel;
-        }
+    }
 
     private void ApplyDragOnGround()
-        {
-        if (_char.isGrounded)
-            {
-
-            Velocity = Velocity * (1 - _drag * Time.deltaTime); //same as lerp
-            }
-        }
+    {
+        Velocity = Velocity * (1 - _drag * Time.deltaTime); //same as lerp
+    }
 
     private void ApplyJump()
+    {
+        if (_jump)
         {
-        if (_char.isGrounded && _jump)
-            {
             Velocity.y += Mathf.Sqrt(2 * Physics.gravity.magnitude * _jumpHeight);
             _jump = false;
-            }
         }
+    }
 
     private void DoMovement()
-        {
-
+    {
         Vector3 movement = Velocity * Time.deltaTime;
         _char.Move(movement);
-        }
+    }
 
     public static float ClampAngle(float angle, float min, float max)
         {
