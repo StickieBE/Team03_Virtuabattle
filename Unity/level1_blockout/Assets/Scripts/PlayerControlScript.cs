cfe99f1bfe36ce6c30d9ff706952c6d9ef44f968
Valentine Vandamme <valentine.vandamme@student.howest.be>
2019-01-08 17:43:52 +0100

    Imported CharSelect UnityPackage

---
+++ Unity/level1_blockout/Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlayerControlScript : MonoBehaviour
+{
+
+    public int playerID = 1;
+    public int controllerID = 1;
+
+    public Material ActiveMat;
+    public Material DeactiveMat;
+    private MeshRenderer meshRenderer;
+
+
+
+    public Text txtInfo;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void Active(int ctrl)
+    {
+        this.enabled = true;
+        controllerID = ctrl;
+
+        meshRenderer.material = ActiveMat;
+
+
+        txtInfo.text = "Press 'B' to back out";
+
+        
+    }
+
+    public void DeActive()
+    {
+        this.enabled = false;
+        meshRenderer.material = DeactiveMat;
+
+        txtInfo.text = "Press 'A' to join";
+
+        
+    }
+
+   
+}
