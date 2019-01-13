ad8d8841c1bbb9bf9d79d4f38f3990f367fadb6e
OscarTulkens <43540793+OscarTulkens@users.noreply.github.com>
2019-01-13 18:17:27 +0100

    Lots of Additions and fixes
    Fixed HealthBar
    Fixed Player Combat and Death
    Fixed TeamCollored Tanks

---
+++ Unity/Team03_Virtuabattle/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+
+    private VariableController _varContr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _varContr = GetComponent<VariableController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_varContr.Health <=0)
+        {
+            Death();
+            
+        }
+    }
+
+    public void Death()
+    {
+        if(_varContr.Gold >= 20)
+        {
+            _varContr.AddHP(50);
+            _varContr.RemoveGold(20);
+            gameObject.transform.position = LevelController.Instance.Spawns[_varContr.Player-1].transform.position;
+
+        }
+
+        else if (_varContr.Gold < 20)
+        {
+            LevelController.Instance.Spawns[_varContr.Player].GetComponent<LoseConditionCheckScript>().Lost();
+        }
+
+    }
+}
+
