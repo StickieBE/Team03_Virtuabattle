9c624a7868274c793824bdd0b63aba39dce32338
Valentine Vandamme <valentine.vandamme@student.howest.be>
2019-01-13 16:44:35 +0100

    added level select UI and controlscreen

---
+++ Unity/Team03_Virtuabattle/Assets/Scripts/StartScreenScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenScript : MonoBehaviour {
+
+public void Play1()
+    {
+       
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Play2()
+    {
+
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void Play3()
+    {
+
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+
+
+
+}
