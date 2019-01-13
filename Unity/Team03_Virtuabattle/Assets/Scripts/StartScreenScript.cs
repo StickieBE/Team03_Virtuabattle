2b5178a8ce0e17bea8594722f3d895cd0e817328
Valentine Vandamme <valentine.vandamme@student.howest.be>
2019-01-13 17:31:30 +0100

    startscreen material

--- Unity/Team03_Virtuabattle/Assets/Scripts/StartScreenScript.cs
+++ Unity/Team03_Virtuabattle/Assets/Scripts/StartScreenScript.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class StartScreenScript : MonoBehaviour {
 
+    public int[] Number;
+
 public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Level()
+    {
+
+        SceneManager.LoadScene("Level" + Number);
+
     }
 
     public void QuitGame()
